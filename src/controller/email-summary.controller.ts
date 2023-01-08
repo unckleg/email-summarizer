2ec5533b58fbddcabaacbad9ae7977e2a05493d7
Djordje Stojiljkovic <djordjestojilljkovic@gmail.com>
2023-01-08 14:36:11 +0100

    Added HTTP Basic Auth and calling it done.

--- src/controller/email-summary.controller.ts
+++ src/controller/email-summary.controller.ts
@@ -32,8 +32,6 @@ export class EmailSummaryController {
     type: SummaryAndSentimentResponseDTO,
   })
   async summarize(@Body(new ValidationPipe({ transform: true })) emailDTO: EmailDTO): Promise<SummaryAndSentimentResponseDTO> {
-    console.log(process.env);
-    console.log(process.env);
     const summaryAndSentimentResponse = await this.sentimentAnalysisService.analyze(emailDTO);
     return plainToClass(SummaryAndSentimentResponseDTO, summaryAndSentimentResponse);
   }
